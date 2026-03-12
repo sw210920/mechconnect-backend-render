f974d028c6f3701c36cf51f9e079f625394d024d
Shubham Wani <sw21092000@gmail.com>
2026-03-12 11:18:30 +0530

    Update CORS configuration to include additional origins

--- src/main/java/com/mechconnect/backend/config/WebConfig.java
+++ src/main/java/com/mechconnect/backend/config/WebConfig.java
@@ -1,13 +1,5 @@
 package com.mechconnect.backend.config;
 
-/**
- * WebConfig
- *
- * Part of the MechConnect backend application.
- * Responsible for handling config related logic.
- */
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -18,17 +10,22 @@ public class WebConfig {
 
     @Bean
     WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // all endpoints
-                        .allowedOrigins("http://127.0.0.1:5500") // your frontend
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://127.0.0.1:5500",   // local frontend
+                                "http://localhost:5500",
+                                "https://mechconnect-frontend-render-tzfy.onrender.com" // deployed frontend
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
-            
-          
         };
     }
 }
